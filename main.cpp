c36119e4a171ab9f8d793c9d933d4e7c702ccc88
cmmamaniqu <cmmamaniqu@unjbg.edu.pe>
2024-08-08 21:11:44 -0500

    prototipo de la función y estructura para almacenar datos main.cpp
    -prototipo y estructura

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void registrarUsuario(); //prototipo de la función
+
+struct Usuario {    // Estructura para almacenar los datos del usuario
+    string username; //cadena para aceptar varias palabras.
+    string password;
+    string userType;
+};
 
