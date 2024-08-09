86d10e6a1cbdbf52d11fc0d7e4ca5c13f4f0ed55
cmmamaniqu <cmmamaniqu@unjbg.edu.pe>
2024-08-08 21:18:57 -0500

    Función principal main y los casos de registro e iniciar sesión Update main.cpp
    -Función main, casos (registro,iniciosesión)

--- main.cpp
+++ main.cpp
@@ -21,3 +21,22 @@ void registrarUsuario() { // Función para registrar un nuevo usuario
     cin >> newUsua[0].password;
     cout << "\nIngrese el tipo de usuario (alumno/docente/admin): ";
  };
+
+int main() {   // Función principal
+    
+	int caso;
+    cout << "1. Registrar nuevo usuario" << endl;  //opciones de caso
+    cout << "2. Iniciar sesion" << endl;
+    cout << "Ingrese una opcion (1 o 2): ";
+    cin >> caso;
+    Usuario lista[20];
+ 
+    lista[0].username =  "juan";
+    lista[0].password =  "123";
+    
+    lista[1].username =  "pedro";
+    lista[1].password =  "234";
+    
+    lista[2].username =  "arturo";
+    lista[2].password =  "135";
+
